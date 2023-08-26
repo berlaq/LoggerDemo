beed13db0d98e8477e82747cd981f1e903c9ea06
Berkay Haberal <haberal54@gmail.com>
2023-08-26 12:09:28 +0300

    Logger Added

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,19 @@
 import express from 'express';
 import bodyParser from "body-parser";
 import {router} from "./routes";
+import {errorHandler} from "./utils/error-handler";
+import {errorMorgan, infoMorgan} from "./utils/morgan-middleware";
 
 const app = express();
 
+app.use(errorMorgan);
+app.use(infoMorgan);
+
 app.use(bodyParser.json());
 
-app.use('/', router);
+app.use('/api', router);
+
+app.use(errorHandler);
 
 app.listen(8080, () => {
     console.log('Server started at port 8080');
--- src/routes/employee-router.ts
+++ src/routes/employee-router.ts
@@ -4,13 +4,13 @@ import {getAllEmployees, getEmployeeById, updateEmployee} from "../controller/em
 export const employeeRouter = Router();
 
 employeeRouter.get('/getAllEmployees', async (req, res, next) => {
-     await getAllEmployees(res, next);
+     await getAllEmployees(res, next).catch(err => next(err));
 });
 
 employeeRouter.get('/getEmployee/:id', async (req, res, next) => {
-    await getEmployeeById(req, res, next);
+    await getEmployeeById(req, res, next).catch(err => next(err));
 });
 
 employeeRouter.patch('/updateEmployee', async (req, res, next) => {
-    await updateEmployee(req, res, next);
+    await updateEmployee(req, res, next).catch(err => next(err));
 });
