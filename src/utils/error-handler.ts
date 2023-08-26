c9d73ecc94a55f53a26e43af08ed66e542891338
Berkay Haberal <haberal54@gmail.com>
2023-08-26 13:00:24 +0300

    Code Improvements

--- src/utils/error-handler.ts
+++ src/utils/error-handler.ts
@@ -1,7 +1,7 @@
 import {ApiError} from "./api-error";
 import express from "express";
 
-export const errorHandler = (err: ApiError | Error, req, res:express.Response) => {
+export const errorHandler = (err: ApiError | Error, req, res:express.Response, next:express.NextFunction) => {
     res.locals.errorId = Math.floor(Math.random() * 1000000);
     const errorStatusCode = err instanceof ApiError ? err.statusCode : 500;
     res.locals.errorMessage = err.message;
